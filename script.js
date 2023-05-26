56f245c635c965147e9f6b3aad92ce34d1e24f29
Christian Gonzales <124095890+cdgonzo23@users.noreply.github.com>
2023-05-25 20:23:04 -0500

    add variables

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log(hello);
+var mealCheck = document.querySelector('#meal-check');
+var drinkCheck = document.querySelector('#meal-check');
+var movieCheck = document.querySelector('#movie-check');
+var searchBtn = document.querySelector('#search-btn');
+var featuredMeal = document.querySelector('#featured-meal');
+var featuredMealTitle = document.querySelector('#featured-meal-title');
+var mealDescription = document.querySelector('#meal-description');
+var mealList = document.querySelector('#meal-list');
+var featuredDrink = document.querySelector('#featured-drink');
+var featuredDrinkTitle = document.querySelector('#featured-drink-title');
+var drinkDescription = document.querySelector('#drink-description');
+var drinkList = document.querySelector('#drink-list');
+var featuredMovie = document.querySelector('#featured-movie');
+var featuredMovieTitle = document.querySelector('#featured-movie-title');
+var movieDescription = document.querySelector('#movie-description');
+var movieList = document.querySelector('#movie-list');
+
+
